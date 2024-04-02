84630394a6ce5fbcb3005d6002d3d69963ca868b
Chovy10 <162932915+Chovy10@users.noreply.github.com>
2024-04-02 18:45:47 +0900

    Add files via upload

---
+++ Lecture04-HW/Lecture04.cpp
@@ -0,0 +1,106 @@
+﻿#pragma comment(lib, "Opengl32.lib")
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+float red = 0.0f;
+float green = 0.0f;
+float blue = 0.0f;
+
+double prevX = 0.0;
+double prevY = 0.0;
+bool isLeftMouseButtonPressed = false;
+
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+
+    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+    {
+        red = 1.0f;
+        green = 0.0f;
+        blue = 0.0f;
+    }
+
+    else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        red = 0.0f;
+        green = 1.0f;
+        blue = 0.0f;
+    }
+
+
+    if (action == GLFW_RELEASE)
+    {
+
+        red = 0.0f;
+        green = 0.0f;
+        blue = 0.0f;
+    }
+
+}
+
+
+void cursorPositionCallback(GLFWwindow* window, double xpos, double ypos)
+{
+    static double prevX = xpos;
+    static double prevY = ypos;
+    bool isStopped = (xpos == prevX && ypos == prevY);
+
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS)
+    {
+        red = 0.0f;
+        green = 0.0f;
+        blue = 1.0f;
+    }
+
+    else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+    {
+        red = 1.0f;
+        green = 0.0f;
+        blue = 1.0f;
+    }
+
+
+
+}
+
+
+
+
+int main(void)
+{
+
+    if (!glfwInit())
+        return -1;
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(1280, 768, "MuSoeunEngine", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+
+    glfwMakeContextCurrent(window);
+
+
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    glfwSetCursorPosCallback(window, cursorPositionCallback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClearColor(red, green, blue, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+
+
+}
