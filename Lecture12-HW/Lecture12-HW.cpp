d5c31c2d436f8a0513c4eb4b960b4d564b862ade
Chovy10 <162932915+Chovy10@users.noreply.github.com>
2024-05-28 14:27:36 +0900

    Add files via upload

---
+++ Lecture12-HW/Lecture12-HW.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <chrono>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+    chrono::steady_clock::time_point start = chrono::steady_clock::now();
+    chrono::steady_clock::time_point end;
+
+    while (true)
+    {
+        end = chrono::steady_clock::now();
+        chrono::steady_clock::duration elapsed_seconds = end - start;
+        if (elapsed_seconds >= chrono::seconds(1))
+        {
+            cout << "while" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
